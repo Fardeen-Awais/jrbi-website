0781e16718b20241fd2447d8c7535d7df0e5326f
fardeen Awais <fardeenjutt99@gmail.com>
2023-04-09 20:33:55 +0500

    Bug in Hero section removed #4

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ function Hero() {
     {/* Introduction */}
     <div className='flex flex-col my-3'>
       <h1 className=' py-3 font-extrabold text-tertiary font-anton'>Social Global <br className='hidden lg:flex' /> Problem</h1>
-      <p className='max-w-xl text-lg my-5 font-normal'>Welcome to Solving Social Global Problem! Our mission is to help individuals improve their psychological wellbeing through our research-backed intervention plan. <br/><br/> Whether you're looking to make lifestyle changes, improve your mental health, or strengthen your relationships, we&apos;re here to support you every step of the way. Contact us today to schedule a meeting and start your journey towards a happier, healthier life.</p>
+      <p className='max-w-xl text-lg my-5 font-normal'>Welcome to Solving Social Global Problem! Our mission is to help individuals improve their psychological wellbeing through our research-backed intervention plan. <br/><br/> Whether you&apos;re looking to make lifestyle changes, improve your mental health, or strengthen your relationships, we&apos;re here to support you every step of the way. Contact us today to schedule a meeting and start your journey towards a happier, healthier life.</p>
 
     </div> 
     
