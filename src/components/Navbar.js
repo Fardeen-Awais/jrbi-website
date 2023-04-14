97b744adb00fdfde09268487b64864cd4d42fda0
Fardeen <fardeenjutt99@gmail.com>
2023-04-14 18:19:46 +0500

    adding navlink in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -305,19 +305,19 @@ function Navbar() {
                     </div>
                   </div>
                   <Link
-                    href="#"
+                    href="/blogs"
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Blogs
                   </Link>
                   <Link
-                    href="#"
+                    href="/assesments"
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Assesments
                   </Link>
                   <Link
-                    href="#"
+                    href="/video"
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-medium leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Video
